17cdbb5ba85863b3d64e74ba03671e226c615278
opilane <opilane@HP-CZC2349HTC>
2025-05-12 11:24:26 +0300

    ,,,

--- funktsioonid.py
+++ funktsioonid.py
@@ -1,106 +1,37 @@
-﻿import smtplib,ssl
-from email.message import EmailMessage
+import json
+import os
 
-def loefailist(fail:str):
-    telefoniraamat=[]
-    with open(fail,'r',encoding="utf-8-sig") as f:
-        for rida in f:
-            nimi,email,telefon=rida.strip().split(",")
-            telefoniraamat.append({'nimi':nimi,'email':email,'telefon':telefon})
-    return telefoniraamat
+faili_nimi="kontaktid.json"
 
-def Kirjutafailisse(fail:str,järjend:list):
-    with open(fail,'w',encoding="utf-8-sig") as f:
-        for k in järjend:
-            f.write(f"{k['nimi']},{k['email']},{k['telefon']}\n")
+def loe_failist():
+    if not os.path.exists(faili_nimi):
+        return[]
+    with open(faili_nimi,'r',encoding="utf-8-sig") as f:
+        return json.load(f)
 
-def lisa_kontakt(telefoniraamat):
-    """Добавление нового контакта в телефонную книгу
-    """
-    print("Lisame uue kontakti!") 
-    uus_nimi = input("Sisesta nimi: ").strip().capitalize()
-    uus_email = input("Sisesta e-mail: ").strip().lower()
-    uus_telefon = input("Sisesta telefoninumber: ").strip()
-    telefoniraamat.append({'nimi': uus_nimi, 'email': uus_email, 'telefon': uus_telefon}) 
-    print("Uus kontakt on lisatud!")
+def salvesta_kontaktid(kontaktid):
+    with open(faili_nimi,'w',encoding="utf-8-sig") as f:
+        json.dump(kontaktid, f, ensure_ascii=False, indent=4)
 
-def kuva_kontaktid(telefoniraamat):
-    """Показывает все контакты в телефонной книге
-    """
-    if telefoniraamat:
-        for k in telefoniraamat:
-            print(k)
+def lisa_kontakt(kontaktid, nimi, telefon,email):
+    kontaktid.append({'nimi':nimi, "telefon": telefon,"email":email})
 
-def otsi_kontakt(telefoniraamat):
-    """Ищет слово во всех значениях словаря и возвращает его запись
-    """
-    nimi=input("Sisesta nimi: ").strip().lower()
-    for kontakt in telefoniraamat:  
-        if nimi in kontakt['nimi'].lower():  
-            print(f"Leitud: {kontakt}") 
-            return kontakt  
-    return None  
+def otsi_kontakt(kontaktid , nimi):
+    return [k for k in kontaktid if nimi.lower() in k['nimi'].lower()]
 
-def paranda_kontakt(telefoniraamat):
-    """Исправление контакта
-    """
-    nimi=input("Sisesta nimi: ").strip().lower()
-    for kontakt in telefoniraamat:  
-        if nimi in kontakt['nimi'].lower():  
-            print(f"Leitud: {kontakt}") 
-            net['nimi']=input("Uus nimi: ")  
-            kirje['email']=input("Uus email: ")  
-            kirje['telefon']=input("Uus telefon: ")  
-        print("Parandatud!")       
-    return None  
+def kustuta_kontakt(kontaktid, nimi):
+    leitud = [k for k in kontaktid if k["nimi"].lower() == nimi.lower()]
+    if leitud:
+        kontaktid.remove(leitud[0])
+        return True
+    return False
 
+def sorteeri_kontaktid(kontaktid, vaike):
+    return sorted(kontaktid, key=lambda x: x[vaike].lower())
 
-
-
-def kustuta_kontakt(telefoniraamat):
-    """ Удалить контакт
-    """
-    nimi=input("Sisesta nimi: ").strip().lower()
-    for kontakt in telefoniraamat:  
-        if nimi in kontakt['nimi'].lower():  
-            print(f"Leitud: {kontakt}") 
-            return kontakt  
-    return None   
-    if kirje:
-        telefoniraamat.remove(kirje)
-        print("Kontakt kustutatud!")
-
-
-def sorteeri_kontakt(telefoniraamat):
-    print("Sorteerimise valikud: nimi / email / telefon")
-    by=input("Sisesta valik: ").lower()
-    if by in ["nimi", "telefon", "email"]:
-        n=len(telefoniraamat)
-        for i in range(n):
-            for j in range(i+1,n):
-                if telefoniraamat[i][by].lower()>telefoniraamat[j][by].lower():
-                    telefoniraamat[i],telefoniraamat[j]=telefoniraamat[j],telefoniraamat[i]
-        print("Kontaktid on sorditud.")
-    else:
-        print("Tundmatu sorterimisviis.")
-
-def saada_kiri():
-    kellele=input("Kellele: ")
-    smtp_server="smtp.gmail.com"
-    smtp_port=587
-    kellelt="kirillgorshkov264@gmail.com"
-    parool=input("Rakenduste parool") #gjfk fbvi nfjk hoyu
-    context=ssl.create_default_context()
-    msg=EmailMessage()
-    msg['Subject']="LOGITpv24"
-    msg['From']=kellelt
-    msg['To']=kellele
-    msg.set_content("ZDAROVA")
-    try:
-        server=smtplib.SMTP(smtp_server,smtp_port)
-        server.starttls(context=context)
-        server.login(kellelt,parool)
-        server.send_message(msg)
-        print('Kiri saadetud')
-    except Exception as e:
-        print("Viga: ", e)
+def muuda_kontakt(kontaktid, vana_nimi ,uus_nimi, uus_telefon, uus_email):
+    for k in kontaktid:
+        if k["nimi"].lower()==vana_nimi.lower():
+            k["nimi"]=uus_nimi
+            k["telefon"]=uus_telefon
+            k["email"]=uus_email
--- main.py
+++ main.py
@@ -1,1 +1,119 @@
-#Telefoniraamat
+import tkinter as tk
+from tkinter import messagebox
+from funktsioonid import *
+
+
+
+
+kontaktid = loe_failist()
+
+def kuva_kontaktid():
+    tekstikast.delete("1.0", "end")
+    for kontakt in kontaktid:
+        tekstikast.insert("end", f"{kontakt['nimi']}| {kontakt['telefon']} | {kontakt['email']}\n")
+
+def lisa_kontakt_gui():
+    nimi = nimi_entry.get()
+    telefon = telefon_entry.get()
+    email = email_entry.get()
+    if nimi and telefon and email:
+        lisa_kontakt(kontaktid, nimi,telefon,email)
+        salvesta_kontaktid(kontaktid)
+        messagebox.showinfo("Edu","kontakt lisatud.")
+        nimi_entry.delete(0,'end')
+        telefon_entry.delete(0,'end')
+        email_entry.delete(0,'end')
+        kuva_kontaktid()
+    else:
+        messagebox.showwarning("Tühjad väljad","Täida kõik väljad")
+
+def otsi_kontakt_gui():
+    nimi = nimi_entry.get()
+    tulemused=otsi_kontakt(kontaktid, nimi)
+    if tulemused:
+        kontakt=tulemused[0]
+        otsingu_viide.set(kontakt["nimi"])
+        nimi_entry.delete(0,'end')
+        nimi_entry.insert(0, kontakt["nimi"])
+        telefon_entry.delete(0,'end')
+        telefon_entry.insert(0, kontakt["telefon"])
+        email_entry.delete(0,'end')
+        email_entry.insert(0, kontakt["email"])
+        tekstikast.delete("1.0",'end')
+        tekstikast.insert("end", f"Leitud: {kontakt['nimi']} | {kontakt['telefon']} | {kontakt['email']}\n")
+    else:
+        messagebox.showwarning("Ei leitud", "Kontakt puudub.")
+
+
+def kustuta_kontakt_gui():
+    nimi=nimi_entry.get()
+    if kustuta_kontakt(kontaktid, nimi):
+        salvesta_kontaktid(kontaktid)
+        messagebox.showinfo("kustutatud", f"'{nimi}' kustutati.")
+        kuva_kontaktid()
+    else:
+        messagebox.showwarning("Ei leitud", "Kontakti ei leitud.")
+
+def sorteeri_gui():
+    kontaktid_sorted=sorteeri_kontaktid(kontaktid, "nimi")
+    tekstikast.delete("1.0", "end")
+    for kontakt in kontaktid_sorted:
+        tekstikast.insert("end", f"{kontakt['nimi']}| {kontakt['telefon']} | {kontakt['email']}\n")
+
+
+def muuda_kontakt_gui():
+    vana_nimi=otsingu_viide.get()
+    uus_nimi=nimi_entry.get()
+    uus_telefon=telefon_entry.get()
+    uus_email=email_entry.get()
+
+    if vana_nimi and uus_nimi and uus_telefon and uus_email:
+        õnnestus=muuda_kontakt(kontaktid,vana_nimi, uus_nimi, uus_telefon, uus_email)
+        if õnnestus:
+            salvesta_kontaktid(kontaktid)
+            messagebox.showinfo("Muudetud", f"'{vana_nimi}' andmed on muudetud.")
+            kuva_kontaktid()
+        else:
+            messagebox.showwarning("Tõrge", "Kontakti ei leitud muudatuseks")
+    else:
+        messagebox.showwarning("Puuduvad andmed","Palun täida kõik muudatuseks.")
+
+
+
+
+
+
+
+aken = tk.Tk()
+aken.title("Telefoniraamat")
+aken.iconbitmap("book.ico")
+aken.configure(bg="orange")
+otsingu_viide=tk.StringVar() #IntVar() #Muudame StringVar-iks, et saaksime salvestada algse nime
+otsingu_viide.set("")
+tk.Label(aken, text="Nimi: ",font=("Calibri",10),fg="black").pack()
+nimi_entry=tk.Entry(aken)
+nimi_entry.pack()
+tk.Label(aken, text="E-mail: ",font=("Calibri",10),fg="black").pack()
+email_entry=tk.Entry(aken)
+email_entry.pack()
+tk.Label(aken, text="Telefon: ",font=("Calibri",10),fg="black").pack()
+telefon_entry=tk.Entry(aken)
+telefon_entry.pack()
+
+nupude_rida=tk.Frame(aken)
+nupude_rida.pack(pady=10)
+
+
+
+tk.Button(nupude_rida, text="Kuva kontaktid", command=kuva_kontaktid,font=("Goudy Stout",5),fg="black").pack(side="left",pady=0)
+tk.Button(nupude_rida, text="Lisa kontakt", command=lisa_kontakt_gui,font=("Goudy Stout",5),fg="black").pack(side="left")
+tk.Button(nupude_rida, text="Otsi kontakt", command=otsi_kontakt_gui,font=("Goudy Stout",5),fg="black").pack(side="left")
+tk.Button(nupude_rida, text="Muuda kontakt", command=muuda_kontakt_gui,font=("Goudy Stout",5),fg="black").pack(side="left")
+tk.Button(nupude_rida, text="Kustuta kontakt", command=kustuta_kontakt_gui,font=("Goudy Stout",5),fg="black").pack(side="left")
+tk.Button(nupude_rida, text="Sorteeri_kontakt", command=sorteeri_gui,font=("Goudy Stout",5),fg="black").pack(side="left")
+
+tekstikast = tk.Text(aken, height=10, width=40)
+tekstikast.pack(pady=12)
+
+
+aken.mainloop()
